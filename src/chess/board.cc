d1dc234f85cef11b3cb9507a487d68716023a2aa
Jason Cleveland <jasonncleveland@gmail.com>
2018-12-22 23:02:32 -0400

    Fixed inheritance logic
    - Fixed piece constructors to use correct inheritence
    - Changed pieces to use specific constructiors

--- src/chess/board.cc
+++ src/chess/board.cc
@@ -40,51 +40,51 @@ void Board::initializePieces()
 
     // Set up Black pieces
     // Rooks
-    (board[0][0])->setPiece(new Piece(black, 'R', vRook));
-    (board[0][7])->setPiece(new Piece(black, 'R', vRook));
+    (board[0][0])->setPiece(new Rook(black));
+    (board[0][7])->setPiece(new Rook(black));
     // Knights
-    (board[0][1])->setPiece(new Piece(black, 'N', vKnight));
-    (board[0][6])->setPiece(new Piece(black, 'N', vKnight));
+    (board[0][1])->setPiece(new Knight(black));
+    (board[0][6])->setPiece(new Knight(black));
     // Bishops
-    (board[0][5])->setPiece(new Piece(black, 'B', vBishop));
-    (board[0][2])->setPiece(new Piece(black, 'B', vBishop));
+    (board[0][5])->setPiece(new Bishop(black));
+    (board[0][2])->setPiece(new Bishop(black));
     // Queen
-    (board[0][3])->setPiece(new Piece(black, 'Q', vQueen));
+    (board[0][3])->setPiece(new Queen(black));
     // King
-    (board[0][4])->setPiece(new Piece(black, 'K', vKing));
+    (board[0][4])->setPiece(new King(black));
     // Pawns
-    (board[1][0])->setPiece(new Piece(black, 'P', vPawn));
-    (board[1][1])->setPiece(new Piece(black, 'P', vPawn));
-    (board[1][2])->setPiece(new Piece(black, 'P', vPawn));
-    (board[1][3])->setPiece(new Piece(black, 'P', vPawn));
-    (board[1][4])->setPiece(new Piece(black, 'P', vPawn));
-    (board[1][5])->setPiece(new Piece(black, 'P', vPawn));
-    (board[1][6])->setPiece(new Piece(black, 'P', vPawn));
-    (board[1][7])->setPiece(new Piece(black, 'P', vPawn));
+    (board[1][1])->setPiece(new Pawn(black));
+    (board[1][0])->setPiece(new Pawn(black));
+    (board[1][2])->setPiece(new Pawn(black));
+    (board[1][3])->setPiece(new Pawn(black));
+    (board[1][4])->setPiece(new Pawn(black));
+    (board[1][5])->setPiece(new Pawn(black));
+    (board[1][6])->setPiece(new Pawn(black));
+    (board[1][7])->setPiece(new Pawn(black));
 
     // Set up White pieces
     // Rooks
-    (board[7][0])->setPiece(new Piece(white, 'R', vPawn));
-    (board[7][7])->setPiece(new Piece(white, 'R', vPawn));
+    (board[7][0])->setPiece(new Rook(white));
+    (board[7][7])->setPiece(new Rook(white));
     // Knights
-    (board[7][1])->setPiece(new Piece(white, 'N', vKnight));
-    (board[7][6])->setPiece(new Piece(white, 'N', vKnight));
+    (board[7][1])->setPiece(new Knight(white));
+    (board[7][6])->setPiece(new Knight(white));
     // Bishops
-    (board[7][2])->setPiece(new Piece(white, 'B', vBishop));
-    (board[7][5])->setPiece(new Piece(white, 'B', vBishop));
+    (board[7][2])->setPiece(new Bishop(white));
+    (board[7][5])->setPiece(new Bishop(white));
     // Queen
-    (board[7][3])->setPiece(new Piece(white, 'Q', vQueen));
+    (board[7][3])->setPiece(new Queen(white));
     // King
-    (board[7][4])->setPiece(new Piece(white, 'K', vKing));
+    (board[7][4])->setPiece(new King(white));
     // Pawns
-    (board[6][0])->setPiece(new Piece(white, 'P', vPawn));
-    (board[6][1])->setPiece(new Piece(white, 'P', vPawn));
-    (board[6][2])->setPiece(new Piece(white, 'P', vPawn));
-    (board[6][3])->setPiece(new Piece(white, 'P', vPawn));
-    (board[6][4])->setPiece(new Piece(white, 'P', vPawn));
-    (board[6][5])->setPiece(new Piece(white, 'P', vPawn));
-    (board[6][6])->setPiece(new Piece(white, 'P', vPawn));
-    (board[6][7])->setPiece(new Piece(white, 'P', vPawn));
+    (board[6][0])->setPiece(new Pawn(white));
+    (board[6][1])->setPiece(new Pawn(white));
+    (board[6][2])->setPiece(new Pawn(white));
+    (board[6][3])->setPiece(new Pawn(white));
+    (board[6][4])->setPiece(new Pawn(white));
+    (board[6][5])->setPiece(new Pawn(white));
+    (board[6][6])->setPiece(new Pawn(white));
+    (board[6][7])->setPiece(new Pawn(white));
 }
 
 void Board::movePiece(Player player, char *move, int *status_code)
@@ -96,55 +96,48 @@ void Board::movePiece(Player player, char *move, int *status_code)
     Square *src_square, *dst_square;
     Piece *piece;
 
-    *status_code = SUCCESS;
-
     // Check if the source square is valid
     if (src_x < 0 || src_x > 7 || src_y < 0 || src_y > 7) {
         *status_code = INVALID_SOURCE_ERROR;
         return;
-    } else {
-        src_square = board[src_x][src_y];
     }
+    src_square = board[src_x][src_y];
 
     // Check if the destination square is valid
     if (dst_x < 0 || dst_x > 7 || dst_y < 0 || dst_y > 7) {
         *status_code = INVALID_DESTINATION_ERROR;
         return;
-    } else {
-        dst_square = board[dst_x][dst_y];
     }
+    dst_square = board[dst_x][dst_y];
 
     // Check if the source square contains a piece
+    piece = src_square->getPiece();
     if (src_square->getPiece() == nullptr) {
         *status_code = INVALID_SOURCE_PIECE_ERROR;
         return;
-    } else {
-        piece = src_square->getPiece();
     }
 
     // Check if the source square contains a valid piece for the current player
     if (src_square->getPiece()->getColour().getColour() != player.getColour().getColour()) {
         *status_code = INVALID_COLOUR_ERROR;
         return;
-    } else {
-        piece = src_square->getPiece();
     }
 
-    // TODO: Check if the piece is allowed to move to the destination
-    /**
-     * For testing I am only checking if a square is empty before I move it
-     * 
-     * Future versions will check if it is a valid move for the piece
-     * Another check will determine if it is capturing anything
-     */
     // Check if the destination contains a piece
     if (dst_square->getPiece() != nullptr) {
         *status_code = OCCUPIED_DESTINATION_ERROR;
         return;
-    } else {
-        dst_square->setPiece(piece);
-        src_square->setPiece(nullptr);
     }
+
+    // Check if the piece is allowed to move to the destination
+    if (!piece->checkMove(src_square->getX(), src_square->getY(), dst_square->getX(), dst_square->getY())) {
+        *status_code = INVALID_MOVE_ERROR;
+        return;
+    }
+
+    *status_code = SUCCESS;
+    dst_square->setPiece(piece);
+    src_square->setPiece(nullptr);
 }
 
 void Board::print()
--- src/chess/piece.hh
+++ src/chess/piece.hh
@@ -4,6 +4,16 @@
 #include <iostream>
 #include "colour.hh"
 
+enum PieceValues
+{
+    vKing = 0,
+    vQueen = 9,
+    vRook = 5,
+    vBishop = 3,
+    vKnight = 3,
+    vPawn = 1
+};
+
 class Piece
 {
     private:
@@ -17,7 +27,7 @@ class Piece
         char getName() {return name;};
         int getValue() {return value;};
 
-        int checkMove(int src_x, int src_y, int dst_x, int dst_y);
+        virtual int checkMove(int src_x, int src_y, int dst_x, int dst_y) {return 1;};
 };
 
 // TODO Implement inherited classes for each piece
@@ -25,53 +35,43 @@ class Piece
 class King : public Piece
 {
     public:
-        King(Colour colour);
-        virtual int checkMove(int src_x, int src_y, int dst_x, int dst_y);
+        King(Colour c) : Piece(c, 'K', vKing) {}
+        int checkMove(int src_x, int src_y, int dst_x, int dst_y);
 };
 
 class Queen : public Piece
 {
     public:
-        Queen(Colour colour);
-        virtual int checkMove(int src_x, int src_y, int dst_x, int dst_y);
+        Queen(Colour c) : Piece(c, 'Q', vQueen) {}
+        int checkMove(int src_x, int src_y, int dst_x, int dst_y);
 };
 
 class Rook : public Piece
 {
     public:
-        Rook(Colour colour);
-        virtual int checkMove(int src_x, int src_y, int dst_x, int dst_y);
+        Rook(Colour c) : Piece(c, 'R', vRook) {}
+        int checkMove(int src_x, int src_y, int dst_x, int dst_y);
 };
 
 class Bishop : public Piece
 {
     public:
-        Bishop(Colour colour);
-        virtual int checkMove(int src_x, int src_y, int dst_x, int dst_y);
+        Bishop(Colour c) : Piece(c, 'B', vBishop) {}
+        int checkMove(int src_x, int src_y, int dst_x, int dst_y);
 };
 
 class Knight : public Piece
 {
     public:
-        Knight(Colour colour);
-        virtual int checkMove(int src_x, int src_y, int dst_x, int dst_y);
+        Knight(Colour c) : Piece(c, 'N', vKnight) {}
+        int checkMove(int src_x, int src_y, int dst_x, int dst_y);
 };
 
 class Pawn : public Piece
 {
     public:
-        Pawn(Colour colour);
-        virtual int checkMove(int src_x, int src_y, int dst_x, int dst_y);
-};
-
-enum PieceValues
-{
-    vKing = 0,
-    vQueen = 9,
-    vRook = 5,
-    vBishop = 3,
-    vKnight = 3,
-    vPawn = 1
+        Pawn(Colour c) : Piece(c, 'P', vPawn) {}
+        int checkMove(int src_x, int src_y, int dst_x, int dst_y);
 };
 
 #endif // CHESS_PIECE_H
